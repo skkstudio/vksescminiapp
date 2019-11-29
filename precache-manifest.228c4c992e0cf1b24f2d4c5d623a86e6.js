69f9b8ff578aa41c165fe4876810fb6a27eb736c
root <ktkozlov@yandex.ru>
2019-11-29 17:12:44 +0500

    Updates

---
+++ precache-manifest.228c4c992e0cf1b24f2d4c5d623a86e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7509e125a440a98660be5baa442d3280",
+    "url": "%7Bskkstudio%7D.github.io/%7Bvksescminiapp%7D/index.html"
+  },
+  {
+    "revision": "d4b979054ccf2efc0ebe",
+    "url": "%7Bskkstudio%7D.github.io/%7Bvksescminiapp%7D/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "4305573fa156605a70ea",
+    "url": "%7Bskkstudio%7D.github.io/%7Bvksescminiapp%7D/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "d4b979054ccf2efc0ebe",
+    "url": "%7Bskkstudio%7D.github.io/%7Bvksescminiapp%7D/static/js/2.6a3ca6ed.chunk.js"
+  },
+  {
+    "revision": "4305573fa156605a70ea",
+    "url": "%7Bskkstudio%7D.github.io/%7Bvksescminiapp%7D/static/js/main.6596dd48.chunk.js"
+  },
+  {
+    "revision": "37664764fae15c9de010",
+    "url": "%7Bskkstudio%7D.github.io/%7Bvksescminiapp%7D/static/js/runtime-main.d66c9e8a.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "%7Bskkstudio%7D.github.io/%7Bvksescminiapp%7D/static/media/persik.4e1ec840.png"
+  }
+]);
